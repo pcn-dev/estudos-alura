ba3f7c8e2b6eaee9e244de30d044df06110644dc
paulo <paulocaetanoneto@gmail.com>
2026-09-13 01:19:57 -0300

    feat: variaveis, tipos primitivos e casting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Os Farofeiros");
 
+        int ano = 2022;
+        System.out.println("Ano do lançamento do file é: " + ano);
+        boolean incluidoPlano = true;
+        double notaFilme = 8.5;
+        // Média feita de acordo com notas de amigos
+        double media = (9.8 + 6.3 + 10) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Os farofeiros
+                Filme de comédia brasileira
+                Nota média: %.2f
+                Ano lançamento: %d
+                """. formatted(media, ano);
+
+        System.out.println(sinopse);
+
+        int nota = (int) (media/2);
+        System.out.println(nota);
+    }
 }
-
-
-
