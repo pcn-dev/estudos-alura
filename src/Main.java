6dcbe9ffd6ff18c48b7d6609e17321727aaa50d6
paulo <paulocaetanoneto@gmail.com>
2026-09-12 23:47:21 -0300

    Aula 1 - Java

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 10) {
+            System.out.println("Estou no while");
+            contador++;
+        }
+
+    }
+
+}
+
+class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Paulo!");
+        System.out.println("Tudo bem?");
+    }
+}
+
+class Estudos{
+    public static void main(String[] args) {
+        System.out.println("Eu vou ser foda em Java!");
+        System.out.println("Eu vou vencer eu mesmo!");
+    }
+}
+
+class Soma{
+    public static void main(String[] args) {
+        int valor1 = 10;
+        int valor2 = 5;
+        System.out.println("Resultado: " + (valor1+valor2));
+    }
+}
+
+class Subtracao{
+    public static void main(String[] args) {
+        int valor1 = 10;
+        int valor2 = 5;
+        System.out.println("Resultado: " + (valor1-valor2));
+    }
+}
